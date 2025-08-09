9ce0e937e27b5a53f3fb6cf76705919c502a8ca8
Sergey <s.pavluchenko90@gmail.com>
2025-08-09 12:36:37 +0300

    added loading

--- src/components/Contacts/Contacts.js
+++ src/components/Contacts/Contacts.js
@@ -9,10 +9,13 @@ import {
   ButtonDel,
   MdDelete,
 } from './ContactsStyled';
+import '../loader/loading.css';
 
 export const Contacts = () => {
   const initialContacts = useSelector(state => state.contacts.contacts.items);
   const filter = useSelector(state => state.filter);
+  const isLoading = useSelector(state => state.contacts.contacts.isLoading);
+
   const dispatch = useDispatch();
 
   const filteredContacts = initialContacts.filter(contact =>
@@ -24,16 +27,20 @@ export const Contacts = () => {
       <h1>Contacts</h1>
       <UlStyle>
         <Filter />
-        <UlStyleContactList>
-          {filteredContacts.map(({ id, name, number }) => (
-            <LiStyle key={id}>
-              {name}: {number}
-              <ButtonDel>
-                <MdDelete onClick={() => dispatch(deleteContactThunk(id))} />
-              </ButtonDel>
-            </LiStyle>
-          ))}
-        </UlStyleContactList>
+        {isLoading ? (
+          <div class="loader"></div>
+        ) : (
+          <UlStyleContactList>
+            {filteredContacts.map(({ id, name, number }) => (
+              <LiStyle key={id}>
+                {name}: {number}
+                <ButtonDel>
+                  <MdDelete onClick={() => dispatch(deleteContactThunk(id))} />
+                </ButtonDel>
+              </LiStyle>
+            ))}
+          </UlStyleContactList>
+        )}
       </UlStyle>
     </div>
   );
